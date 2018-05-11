14d43a8395851ee869631098fe5b947746a7bdce
Mohammed Abdelbarry <m7md337@gmail.com>
2018-05-11 19:35:07 +0200

    Added object code for relops

--- bytecode.h
+++ bytecode.h
@@ -9,10 +9,18 @@
 /* Variable Storing */
 #define ISTORE "istore"
 #define FSTORE "fstore"
+#define LDC "ldc"
 
+/* Relational Operations */
+#define IFCMP "ifcmp_"
+
+/* Arithmetic Operations */
 #define IADD "iadd"
 #define ISUB "isub"
 #define IMUL "imul"
 #define IDIV "idiv"
+
+/* Jump Instructions */
+#define GOTO "goto"
 
 #endif
